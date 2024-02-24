5a6232c2d3bd4312fb5104fb559bf24c587a10ac
FraI3mega <projeapp2@gmail.com>
2024-02-24 12:14:43 +0100

    add codesnap.nvim

---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,9 @@
+return {
+  "mistricky/codesnap.nvim",
+  cmd = { "CodeSnapPreviewOn" },
+  build = "make",
+  opts = {
+    watermark = "", -- (Optional) you can custom your own watermark, but if you don't like it, just set it to ""
+    editor_font_family = "Fira Code SemiBold",
+  },
+}
