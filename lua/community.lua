2bc96ab2459825d828922830295dce47f2effbe3
FraI3mega <projeapp2@gmail.com>
2024-02-26 19:05:12 +0100

    add nix

--- lua/community.lua
+++ lua/community.lua
@@ -28,6 +28,7 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.nix" },
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },
