5ac468953742c7dc13c7556442c7f7ccc56e8f2d
FraI3mega <projeapp2@gmail.com>
2024-03-13 20:00:23 +0100

    fix icon picker 2

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -5,7 +5,12 @@ return {
     cmd = { "Cheat", "CheatWithoutComments" },
   },
   { "wakatime/vim-wakatime", lazy = false },
-  { "ziontee113/icon-picker.nvim", cmd = { "IconPickerNormal", "IconPickerYank" } },
+  {
+    "ziontee113/icon-picker.nvim",
+    cmd = { "IconPickerNormal", "IconPickerYank", "IconPickerInsert" },
+    config = { disable_legacy_commands = true },
+    keys = { { "<C-i>", "<cmd>IconPickerInsert<cr>", mode = "i", desc = "Insert from icon picker" } },
+  },
   {
     "2kabhishek/tdo.nvim",
     dependencies = "nvim-telescope/telescope.nvim",
