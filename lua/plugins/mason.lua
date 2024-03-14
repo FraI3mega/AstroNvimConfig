12bbceb175db48aeec22c1aca047e0e71d1cc5da
FraI3mega <projeapp2@gmail.com>
2024-03-14 22:32:20 +0100

    add slint_lsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -12,7 +12,8 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(
         opts.ensure_installed,
-        "lua_ls"
+        "lua_ls",
+        "slint_lsp"
         -- add more arguments for adding more language servers
       )
     end,
