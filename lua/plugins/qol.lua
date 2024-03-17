9cd3a056f58f4dfa4d8520c2160eb688ffc179c3
FraI3mega <projeapp2@gmail.com>
2024-03-17 22:31:23 +0100

    fix fow windows

--- lua/plugins/qol.lua
+++ lua/plugins/qol.lua
@@ -19,45 +19,45 @@ return {
       snippetDir = "./snippets",
     },
   },
-  { -- This plugin
-    "Zeioth/compiler.nvim",
-    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
-    dependencies = { "stevearc/overseer.nvim" },
-    opts = {},
-  },
-  { -- The task runner we use
-    "stevearc/overseer.nvim",
-    commit = "68a2d344cea4a2e11acfb5690dc8ecd1a1ec0ce0",
-    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
-    opts = {
-      task_list = {
-        direction = "bottom",
-        min_height = 25,
-        max_height = 25,
-        default_detail = 1,
-      },
-    },
-  },
-  {
-    "mistricky/codesnap.nvim",
-    cmd = { "CodeSnap", "CodeSnapSave" },
-    build = "make",
-    opts = {
-      watermark = "", -- (Optional) you can custom your own watermark, but if you don't like it, just set it to ""
-      save_path = "./code.png",
-      has_breadcrumbs = true,
-    },
-  },
-  {
-    "HakonHarnes/img-clip.nvim",
-    event = "BufEnter",
-    opts = {
-      -- add options here
-      -- or leave it empty to use the default settings
-    },
-    cmd = "PasteImage",
-    -- suggested keymap
-  },
+  -- { -- This plugin
+  --   "Zeioth/compiler.nvim",
+  --   cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+  --   dependencies = { "stevearc/overseer.nvim" },
+  --   opts = {},
+  -- },
+  -- { -- The task runner we use
+  --   "stevearc/overseer.nvim",
+  --   commit = "68a2d344cea4a2e11acfb5690dc8ecd1a1ec0ce0",
+  --   cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+  --   opts = {
+  --     task_list = {
+  --       direction = "bottom",
+  --       min_height = 25,
+  --       max_height = 25,
+  --       default_detail = 1,
+  --     },
+  --   },
+  -- },
+  -- {
+    -- "mistricky/codesnap.nvim",
+    -- cmd = { "CodeSnap", "CodeSnapSave" },
+    -- build = "make",
+    -- opts = {
+    --   watermark = "", -- (Optional) you can custom your own watermark, but if you don't like it, just set it to ""
+    --   save_path = "./code.png",
+    --   has_breadcrumbs = true,
+    -- },
+  -- },
+  -- {
+  --   "HakonHarnes/img-clip.nvim",
+  --   event = "BufEnter",
+  --   opts = {
+  --     -- add options here
+  --     -- or leave it empty to use the default settings
+  --   },
+  --   cmd = "PasteImage",
+  --   -- suggested keymap
+  -- },
   {
     "mcauley-penney/visual-whitespace.nvim",
     event = "BufEnter",
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,6 +10,7 @@ return {
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
       "vim",
+      "python"
       -- add more arguments for adding more treesitter parsers
     })
   end,
