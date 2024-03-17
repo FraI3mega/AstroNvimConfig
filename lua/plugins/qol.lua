fd0487d2930a5d453afb3db2adb1136320f653c3
FraI3mega <projeapp2@gmail.com>
2024-03-17 14:29:32 +0100

    fix codesnap

--- lua/plugins/qol.lua
+++ lua/plugins/qol.lua
@@ -40,11 +40,11 @@ return {
   },
   {
     "mistricky/codesnap.nvim",
-    cmd = { "CodeSnapPreviewOn" },
+    cmd = { "CodeSnap", "CodeSnapSave" },
     build = "make",
     opts = {
       watermark = "", -- (Optional) you can custom your own watermark, but if you don't like it, just set it to ""
-      editor_font_family = "Fira Code SemiBold",
+      save_path = "./code.png",
     },
   },
   {
