63863ca0c61f493ca67ed3131d66ce8fc65ffb0e
FraI3mega <projeapp2@gmail.com>
2024-04-04 21:02:43 +0200

    move to community versions

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -4,17 +4,11 @@ return {
     dependencies = "RishabhRD/popfix",
     cmd = { "Cheat", "CheatWithoutComments" },
   },
-  { "wakatime/vim-wakatime", lazy = false },
   {
     "ziontee113/icon-picker.nvim",
     cmd = { "IconPickerNormal", "IconPickerYank", "IconPickerInsert" },
     config = { disable_legacy_commands = true },
     keys = { { "<C-i>", "<cmd>IconPickerInsert<cr>", mode = "i", desc = "Insert from icon picker" } },
-  },
-  {
-    "2kabhishek/tdo.nvim",
-    dependencies = "nvim-telescope/telescope.nvim",
-    cmd = { "Tdo", "TdoEntry", "TdoNote", "TdoTodos", "TdoToggle", "TdoFind", "TdoFiles" },
   },
   {
     "cpea2506/relative-toggle.nvim",
