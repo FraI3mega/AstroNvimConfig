cee84f340e38b9a6ce04210994f52fa634d7a2b0
FraI3mega <projeapp2@gmail.com>
2024-04-09 16:39:47 +0200

    unify

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -21,4 +21,28 @@ return {
       plugin_manager = "lazy",
     },
   },
+  {
+    "chrisgrieser/nvim-scissors",
+    dependencies = "nvim-telescope/telescope.nvim", -- optional
+    cmd = { "ScissorsAddNewSnippet", "ScissorsEditSnippet" },
+    opts = {
+      snippetDir = "./snippets",
+    },
+  },
+  {
+    "HakonHarnes/img-clip.nvim",
+    event = "BufEnter",
+    opts = {
+      -- add options here
+      -- or leave it empty to use the default settings
+    },
+    cmd = "PasteImage",
+    -- suggested keymap
+  },
+  {
+    "mcauley-penney/visual-whitespace.nvim",
+    event = "BufEnter",
+    config = true,
+    opts = { highlight = { link = "Visual" } },
+  },
 }
