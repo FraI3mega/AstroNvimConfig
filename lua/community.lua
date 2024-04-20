a124f98f200067a98fe8ef624589cb862e808100
FraI3mega <projeapp2@gmail.com>
2024-04-20 14:33:56 +0200

    set oil.nvim as deafult file manager

--- lua/community.lua
+++ lua/community.lua
@@ -17,7 +17,7 @@ return {
   { import = "astrocommunity.editing-support.undotree" },
   { import = "astrocommunity.editing-support.zen-mode-nvim" },
   { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
-  { import = "astrocommunity.file-explorer.oil-nvim" },
+  -- { import = "astrocommunity.file-explorer.oil-nvim" },
   { import = "astrocommunity.git.diffview-nvim" },
   { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.lsp.garbage-day-nvim" },
